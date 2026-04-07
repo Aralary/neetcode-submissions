4ae80d4822b89c2452f23298fe524f23da52af46
Aralary <70821464+Aralary@users.noreply.github.com>
2026-04-07 17:21:01 +0300

    Add: valid-binary-search-tree - submission-1

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-1.go
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func isValidBST(root *TreeNode) bool {
+	return validate(root, -1001, 1001)
+}
+
+func validate(root *TreeNode, min, max int) bool {
+	if root == nil {
+		return true
+	}
+	if root.Val <= min || root.Val >= max {
+		return false
+	}
+	return validate(root.Left, min, root.Val) && validate(root.Right, root.Val, max)
+}
