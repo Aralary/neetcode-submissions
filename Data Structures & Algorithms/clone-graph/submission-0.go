9ee156bed0d7a41c71fd9391848b4f672cb14acb
Aralary <70821464+Aralary@users.noreply.github.com>
2026-04-21 15:26:58 +0300

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.go
@@ -0,0 +1,33 @@
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Neighbors []*Node
+ * }
+ */
+
+func cloneGraph(node *Node) *Node {
+    if node == nil {
+        return nil
+    }
+    visited := make(map[*Node]*Node)
+
+    var dfs func(n *Node) *Node
+    dfs = func(n *Node) *Node {
+        if n == nil {
+            return nil
+        }
+        if clone, ok := visited[n]; ok {
+            return clone
+        }
+        clone := &Node{Val: n.Val}
+        visited[n] = clone
+
+        for _, neighbor := range n.Neighbors {
+            clone.Neighbors = append(clone.Neighbors, dfs(neighbor))
+        }
+        return clone
+    }
+
+    return dfs(node)
+}
